0cac7057019e1f036f26573b61ae8ab99eac7a9d
yasminahmed01 <yasmin180946@feng.bu.edu.eg>
2023-03-30 20:26:17 +0200

    Create-Send/Cancel-Request endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/signup", userController.signUp);
-router.post("/signin/", userController.signIn);
+router.post("/signin/", userController.signIn); 
+router.post('/sendrequest/:ID/', guard.isLogin, userController.sendRequest);
+router.post('/cancelrequest/:ID/', guard.isLogin, userController.cancelRequest);
 
 module.exports = router
