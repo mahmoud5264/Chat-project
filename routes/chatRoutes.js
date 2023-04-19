ca6ba341807303d8117465296b182d027249e28e
Mohamed Abdelghani <mohamedabdelghani643@gmail.com>
2023-04-20 00:49:33 +0200

    createChat-getChat-reName

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,5 +5,8 @@ const upload = require("../utils/multer")
 const route = require('express').Router()
 route.get('/allChats', isLogin, allChats)
 route.get('/members/:id', isLogin, chatMemebers)
+route.get('/getChat/:id', isLogin, getChat);
 route.post('/pinned', isLogin, allPinnedChats)
+route.post('/create', upload.multerProfile.single('image'), isLogin, createChat)
+route.post('/rename', isLogin, changeName)
 module.exports = route
